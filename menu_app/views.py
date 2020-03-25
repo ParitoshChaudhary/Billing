12d84b75101e50386a61798d6fe70e7f65ff3a6f
Paritosh Chaudhary <paritoshc.at@gmail.com>
2020-03-25 20:15:13 +0530

    Added edit option and pagination. Dated 25.03.2020

--- menu_app/views.py
+++ menu_app/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from menu_app.models import Item as ItemList, Category, Cuisine
 from menu_app.forms import ItemForm, CategoryForm, CuisineForm
 from django.contrib import messages
+from django.core.paginator import Paginator
 
 
 # Create your views here.
@@ -16,6 +17,9 @@ def menu_list(request):
         'category': category,
         'cuisine': cuisine
     }
+    paginator = Paginator(item_list, 10)
+    page = request.GET.get('pg')
+    item_list = paginator.get_page(page)
     return render(request, 'menu_list.html', context)
 
 
@@ -46,8 +50,11 @@ def add_item(request):
         item_list = ItemList.objects.all()
         cuisine = Cuisine.objects.all()
         categories = Category.objects.all()
+        paginator = Paginator(item_list, 10)
+        page = request.GET.get('pg')
+        items = paginator.get_page(page)
         context = {
-            'items': item_list,
+            'items': items,
             'categories' : categories,
             'cuisine' : cuisine
         }
@@ -56,12 +63,31 @@ def add_item(request):
 
 def edit_item(request, item_id):
     if request.method == 'POST':
-        pass
+        item = ItemList.objects.get(pk=item_id)
+        itemform = ItemForm(request.POST, instance = item)
+        cuisineform = CuisineForm(request.POST, instance = item.cuisine)
+        catform = CategoryForm(request.POST, instance = item.category)
+        if itemform.is_valid():
+            item = itemform.save(commit=False)
+            cui = cuisineform.save(commit=False)
+            cat = catform.save(commit=False)
+
+            item.cuisine = Cuisine.objects.get(cuisine=cui)
+            item.category = Category.objects.get(category_type=cat)
+            item.name = request.POST.get('name')
+            item.cost = request.POST.get('cost')
+            item.save()
+        messages.success(request, 'Item edited successfully.')
+        return redirect('add_item')
 
     else:
         item_name = ItemList.objects.get(pk=item_id)
+        cuisine = Cuisine.objects.all()
+        categories = Category.objects.all()
         context = {
-            'items': item_name
+            'items': item_name,
+            'categories' : categories,
+            'cuisine' : cuisine
         }
         return render(request, 'edit.html', context)
 
